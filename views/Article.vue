dc2bf3702065bc732017ba63471a34779a6d4c03
kexin.an <kexin.an@thoughtworks.com>
2023-03-31 13:07:59 +0800

    feat:add comment in article view

--- views/Article.vue
+++ views/Article.vue
@@ -5,7 +5,14 @@
       <n-h5>发表于：{{ formatDate(article.createdAt) }}</n-h5>
     </n-space>
     <n-divider/>
-    <div v-html="article.content"></div>
+    <div v-html="article.content" style="margin-bottom: 1.5rem"></div>
+    <n-divider/>
+    <n-h2 style="font-size: 35px;">评论</n-h2>
+    <n-card v-for="(comment,index) in comments" :key="index" class="comment">
+      <n-h4 >{{ comment.userName }}</n-h4>
+      {{ comment.content }}
+    </n-card>
+    <n-pagination v-model:page="page" :page-count="totalPages" @page-change="(newPage) => page = newPage"/>
   </n-card>
 </template>
 
@@ -15,18 +22,46 @@ import axios from "axios";
 export default {
   data() {
     return {
-      article: []
+      article: [],
+      comments:[],
+      totalPages:0,
+      pageNumber:0,
+      page:1,
+      loading:false
     };
   },
   created() {
     const articleId = this.$route.params.articleId;
-    axios.get(`http://localhost:8080/articles/${articleId}`).then((response) => {
-      this.article = response.data;
+    axios.get(`http://localhost:8080/articles/${articleId}`)
+        .then(response => {
+          this.article = response.data;
+        });
+    const getPage = (newPage) => {
+      this.loading = true;
+      axios.get(`http://localhost:8080/comments/${articleId}?page=${newPage - 1}`)
+          .then(response => {
+            this.comments = response.data.content;
+            this.totalPages = response.data.totalPages;
+            this.pageNumber = response.data.number + 1;
+          })
+          .catch(error => {
+            console.log(error);
+          })
+          .finally(() => {
+            this.loading = false;
+          });
+    }
+    getPage(this.page);
+    this.$watch('page', newPage => {
+      if (!this.loading) {
+        getPage(newPage);
+      }
     });
   },
+
   methods: {
     formatDate(timeStamp) {
-      const date = new Date(timeStamp );
+      const date = new Date(timeStamp);
       const year = date.getFullYear();
       const month = ("0" + (date.getMonth() + 1)).slice(-2);
       const day = ("0" + date.getDate()).slice(-2);
@@ -54,5 +89,8 @@ export default {
   font-size: 40px;
 }
 
+.comment {
+  margin: 5px 0;
+}
 
 </style>
